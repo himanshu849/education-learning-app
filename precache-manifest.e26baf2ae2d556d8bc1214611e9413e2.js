271c273f173783ee1697cb3ea40a20896bec1c7c
himanshu8496 <eeehimanshu22@gmail.com>
2020-06-05 12:33:54 +0530

    Updates

---
+++ precache-manifest.e26baf2ae2d556d8bc1214611e9413e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2946a8eecef03f0fcf4d35271352f738",
+    "url": "/education-learning-zeiss-app/index.html"
+  },
+  {
+    "revision": "c99d52198ce687048309",
+    "url": "/education-learning-zeiss-app/static/css/main.1363f571.chunk.css"
+  },
+  {
+    "revision": "0035ec487b2813231ab3",
+    "url": "/education-learning-zeiss-app/static/js/2.4c5c6660.chunk.js"
+  },
+  {
+    "revision": "f8a0becf11b005379cd9a64a24caeac6",
+    "url": "/education-learning-zeiss-app/static/js/2.4c5c6660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99d52198ce687048309",
+    "url": "/education-learning-zeiss-app/static/js/main.13705bc4.chunk.js"
+  },
+  {
+    "revision": "11b00f04463b2052d89f",
+    "url": "/education-learning-zeiss-app/static/js/runtime-main.51b382c1.js"
+  },
+  {
+    "revision": "70b583d651d61160187224ff47d93cab",
+    "url": "/education-learning-zeiss-app/static/media/gone.70b583d6.mp4"
+  },
+  {
+    "revision": "3d8ed9bdc000d0e61d22b83b998061b0",
+    "url": "/education-learning-zeiss-app/static/media/lake.3d8ed9bd.jpg"
+  },
+  {
+    "revision": "bfd19c4e7ad04c72bfb327cf7e6b9576",
+    "url": "/education-learning-zeiss-app/static/media/trailer_hd.bfd19c4e.mp4"
+  }
+]);
