7e2ee8eaa214a9a413c00cb2ea4ee1df56c34046
himanshu8496 <eeehimanshu22@gmail.com>
2020-06-05 12:59:21 +0530

    Updates

---
+++ precache-manifest.709f971615757d540637a3185b333866.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec56b3955953cef52db700ce7c3ff4aa",
+    "url": "/education-learning-app/index.html"
+  },
+  {
+    "revision": "065b76ba1d5aeb6aaa8f",
+    "url": "/education-learning-app/static/css/main.1363f571.chunk.css"
+  },
+  {
+    "revision": "a41adaab45048faa3dec",
+    "url": "/education-learning-app/static/js/2.ef9a3628.chunk.js"
+  },
+  {
+    "revision": "f8a0becf11b005379cd9a64a24caeac6",
+    "url": "/education-learning-app/static/js/2.ef9a3628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065b76ba1d5aeb6aaa8f",
+    "url": "/education-learning-app/static/js/main.4053a9d4.chunk.js"
+  },
+  {
+    "revision": "b2e1f668b3d71260f677",
+    "url": "/education-learning-app/static/js/runtime-main.ca6cef49.js"
+  },
+  {
+    "revision": "70b583d651d61160187224ff47d93cab",
+    "url": "/education-learning-app/static/media/gone.70b583d6.mp4"
+  },
+  {
+    "revision": "3d8ed9bdc000d0e61d22b83b998061b0",
+    "url": "/education-learning-app/static/media/lake.3d8ed9bd.jpg"
+  },
+  {
+    "revision": "bfd19c4e7ad04c72bfb327cf7e6b9576",
+    "url": "/education-learning-app/static/media/trailer_hd.bfd19c4e.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.d161ca8fed4b987c8c71511da828b3d8.js"
+  "/education-learning-app/precache-manifest.709f971615757d540637a3185b333866.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/education-learning-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
